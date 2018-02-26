743f0216c934eb4b7675f1f3046a5069bb62688f
divshah <divshah@paypal.com>
2018-02-26 00:45:58 -0800

    end point changed

--- controllers/index.js
+++ controllers/index.js
@@ -14,7 +14,7 @@ module.exports = function (router) {
         
     // });
 
-    router.get('/', function (req, res){
+    router.get('/webhook', function (req, res){
         const hubChallenge = req.query['hub.challenge'];
         const hubMode = req.query['hub.mode'];
          const verifyTokenMatches = (req.query['hub.verify_token'] === 'contento');
