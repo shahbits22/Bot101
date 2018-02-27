48e0aadab6436783287f21fc33ee87fa2d65af6f
divshah <divshah@paypal.com>
2018-02-27 14:45:44 -0800

    configured tokens

--- helpers/processMessage.js
+++ helpers/processMessage.js
@@ -1,6 +1,6 @@
-const API_AI_TOKEN = '41cfa157af724790b4226acf50647c0e';
+const API_AI_TOKEN = process.env.API_AI_TOKEN;
 const apiAiClient = require('apiai')(API_AI_TOKEN);
-const FACEBOOK_ACCESS_TOKEN = 'EAACXpb5AwksBABVVLA9cMgnezjJ2ub2dA3HypTflLZAnFgxW7zei5SWgDjjXUTZAbGwhN7cAKCUsyDjVJbJf8FXn3JBRbZBNnkatHmwX5QjReQilpfbq36zZBm5ZBDFOZCC7V68Ch1rQ6kBse8shXWGfTLHHyuwgjpZCj7uV6rY0wZDZD';
+const FACEBOOK_ACCESS_TOKEN = process.env.FACEBOOK_ACCESS_TOKEN;
 const request = require('request');
 
 const sendTextMessage = (senderId, text) => {
