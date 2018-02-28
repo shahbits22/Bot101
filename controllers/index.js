338db8584ffb77e3734ba94081d2da0be878cc4f
divshah <divshah@paypal.com>
2018-02-28 03:17:54 -0800

    Fixed bug

--- controllers/index.js
+++ controllers/index.js
@@ -5,8 +5,6 @@ const MessageHelper = require('../helpers/messageHelper');
 
 module.exports = function (router) {
 
-    var model = new IndexModel();
-
     // router.get('/', function (req, res) {
         
     //     res.send('<code><pre>' + JSON.stringify(model, null, 2) + '</pre></code>');
