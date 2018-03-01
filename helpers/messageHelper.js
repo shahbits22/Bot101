9d2a5c3efceb4097eee6f34220a54d7d0e2d71bf
divshah <divshah@paypal.com>
2018-02-28 23:20:40 -0800

    Added Persistant menu

--- helpers/messageHelper.js
+++ helpers/messageHelper.js
@@ -4,6 +4,35 @@ const apiAiClient = require('apiai')(API_AI_TOKEN);
 const FACEBOOK_ACCESS_TOKEN = process.env.FACEBOOK_ACCESS_TOKEN;
 const FACEBOOK_API_URL = 'https://graph.facebook.com/v2.6/me';
 const GET_STARTED = 'Get started';
+const MANAGE_PRESCRIPTION = 'Manage prescription';
+const PERSISTANT_MENU = {
+    "persistent_menu":[
+      {
+        "locale":"default",
+        "composer_input_disabled": false,
+        "call_to_actions":[
+            {
+            "title":"Register Account",
+            "type":"web_url",
+            "url":"https://www.paypal.com/welcome/signup/#/email_password",
+            "webview_height_ratio":"full"
+            },
+            {
+            "type":"postback",
+            "title":"Manage Prescriptions",
+            "payload":MANAGE_PRESCRIPTION
+            },
+            {
+                "title":"Update Account",
+                "type":"web_url",
+                "url":"https://www.paypal.com/welcome/signup/#/email_password",
+                "webview_height_ratio":"full"
+            }
+        ]
+
+      }
+    ]
+  }
 
 
 const sendTextMessage = (senderId, text) => {
@@ -62,11 +91,7 @@ export const setupPersistantMenu = () => {
         url: `${FACEBOOK_API_URL}/messenger_profile`,
         qs: { access_token: FACEBOOK_ACCESS_TOKEN },
         method: 'POST',
-        json: {
-            "get_started":{
-                "payload":GET_STARTED
-              }
-            }
+        json: PERSISTANT_MENU
         });
 }
 
